c4517975122ac03119048bdd617a7519afcb3a6b
Aidan Pilgrim <aidan@Aidans-MacBook-Air.local>
2022-08-03 14:35:48 +0100

    working api and tests

--- tests/test_get_esi/test_get_concurrent.py
+++ tests/test_get_esi/test_get_concurrent.py
@@ -1,14 +1,12 @@
 
 from region_runner.get_esi.get_concurrent import get_concurrent_reqs, get_first_page
 
-
-
-def test_get_concurrent_reqs():
-    result = get_concurrent_reqs()
-
-    assert result
-
 def test_get_first_page():
     result = get_first_page()
 
     assert result
+
+def test_get_concurrent_reqs():
+    result = get_concurrent_reqs()
+    assert result
+
