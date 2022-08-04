bad88b39aa80a16f901be8996bd34d53c8b712e2
Aidan Pilgrim <aidan@Aidans-MacBook-Air.local>
2022-08-04 09:45:47 +0100

    working dynamic 'show' query

--- region_runner/__init__.py
+++ region_runner/__init__.py
@@ -31,7 +31,8 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
-    from . import get_data
-    app.register_blueprint(get_data.bp)
+    from . import data
+    app.register_blueprint(data.bp)
+    
 
     return app
