20478ff63a33db26e6c0d93b551b9ea3ef3094dd
Aidan Pilgrim <aidan@Aidans-MacBook-Air.local>
2022-08-10 17:09:37 +0100

    latest

--- region_runner/data.py
+++ region_runner/data.py
@@ -54,15 +54,15 @@ def get_data():
 @bp.route('/show-data', methods=('GET', 'POST'))
 def show_data():
     db = get_db()
+    message = None
     stations = db.execute("""SELECT * FROM stations""").fetchall()
-
     if request.method == 'POST':
         error = None
         with current_app.open_resource('static/query/dynam_arb_query.sql') as f:
             query = f.read().decode('utf8')
 
         from_id = 60003760 # request.form['from_address'] 
-        to_id = 1039149782071 # request.form['to_address']
+        to_id = 60015070 # request.form['to_address']
 
         if not from_id:
             error = 'Missing from_id'
@@ -75,6 +75,7 @@ def show_data():
             error = er
 
         if error == None:
+            message = res
             orders = []
             for r in res:
                 o = {}
@@ -85,9 +86,9 @@ def show_data():
                 o['base_margin'] = r['base_margin']
                 o['volume'] = r['volume']
                 orders.append(o)
-
+            flash(message)
             return render_template('/data/show-data.html', stations= stations, orders=orders)
 
-        flash(error)
+    
 
     return render_template('/data/show-data.html', stations= stations)
