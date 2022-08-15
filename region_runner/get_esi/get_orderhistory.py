158454590c21cfcd49531fe139c9f0596bf55ff6
Aidan Pilgrim <aidan@Aidans-MacBook-Air.local>
2022-08-15 17:56:25 +0100

    latest

--- region_runner/get_esi/get_orderhistory.py
+++ region_runner/get_esi/get_orderhistory.py
@@ -1,7 +1,5 @@
-from requests.auth import HTTPBasicAuth
 from requests.exceptions import HTTPError
-import grequests, requests, os, datetime
-from common import cache
+import grequests, datetime
 from region_runner.db import get_db
 import pandas as pd
 import itertools as it
@@ -15,8 +13,6 @@ def get_concurrent_orderhistory(pairs):
 
     url = 'https://esi.evetech.net/latest/markets/{}/history/?datasource=tranquility&type_id={}'
     reqs = [url.format(p[0][0], p[1][0]) for p in pairs]
-    print(len(reqs))
-    
     rs = (grequests.get(r) for r in reqs)
     responses = grequests.map(rs)
 
@@ -37,7 +33,7 @@ def get_concurrent_orderhistory(pairs):
 def fetch_order_history():
     db = get_db()
     regions_sql = db.execute("""SELECT regionID FROM regions""").fetchall()
-    types_sql = db.execute("""SELECT typeID FROM types WHERE published=1 Limit 5""").fetchall()
+    types_sql = db.execute("""SELECT typeID FROM types WHERE published=1""").fetchall()
     regions = [list(i) for i in regions_sql]
     types = [list(i) for i in types_sql]
     pairs = it.product(regions, types)
@@ -46,10 +42,12 @@ def fetch_order_history():
     date_time = str(datetime.datetime.now())
 
     # refac to try/except to make clear it's an error
-    if orders:
+    try:
         df = pd.DataFrame(orders)
         df = df.assign(extracted_timestamp=date_time)
         df.to_sql('order_history', con=db, if_exists='append')
+    except:
+        print('Failed to copy to Dataframe')
 
 #add to cli
 @click.command('fetch-order-hist')
