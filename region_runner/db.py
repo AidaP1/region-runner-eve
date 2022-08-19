ab60904b5f3372540b62803cf3720cd9e35cda17
Aidan Pilgrim <aidan@Aidans-MacBook-Air.local>
2022-08-19 14:15:55 +0100

    refactor the insert q

--- region_runner/db.py
+++ region_runner/db.py
@@ -5,17 +5,38 @@ import psycopg2
 import click
 import pandas as pd
 from flask import current_app, g
-import sqlalchemy
 
 def get_db():
     if 'db' not in g:
         DATABASE_URL = os.environ['DATABASE_URL']
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
         g.db = psycopg2.connect(
             DATABASE_URL, 
             sslmode='require')
-        g.conn = g.db.connect().connection
 
     return g.db
+
+def execute_values(conn, df, table):
+    """
+    Using psycopg2.extras.execute_values() to insert the dataframe
+    """
+    # Create a list of tupples from the dataframe values
+    tuples = [tuple(x) for x in df.to_numpy()]
+    # Comma-separated dataframe columns
+    cols = ','.join(list(df.columns))
+    # SQL quert to execute
+    query  = "INSERT INTO %s(%s) VALUES %%s" % (table, cols)
+    cursor = conn.cursor()
+    try:
+        psycopg2.extras.execute_values(cursor, query, tuples)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error: %s" % error)
+        conn.rollback()
+        cursor.close()
+        return 1
+    print("execute_values() done")
+    cursor.close()
 
 
 def close_db(e=None):
@@ -36,7 +57,8 @@ def fetch_stations():
     
     try:
         data = pd.read_csv(url)
-        data.to_sql('stations', con=g.conn, if_exists='replace')
+        execute_values(db, data, 'structures')
+        # data.to_sql('stations', con=g.conn, if_exists='replace')
     except db.Error as er:
         return er
 
